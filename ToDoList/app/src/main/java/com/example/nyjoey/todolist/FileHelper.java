a2716339f122e3d253a91801555b50b28012338e
Joey Cheung <joeycheung0@gmail.com>
2018-08-22 12:36:13 -0400

    First android project with a simple to do list app!

---
+++ ToDoList/app/src/main/java/com/example/nyjoey/todolist/FileHelper.java
@@ -0,0 +1,51 @@
+package com.example.nyjoey.todolist;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FileHelper {
+
+    public static final String FILENAME ="listinfo.dat";
+
+    public static void writeData(ArrayList<String> items, Context context) {
+
+        try {
+            FileOutputStream fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(items);
+            oos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<String> readData(Context context) {
+        ArrayList<String> itemsList = null;
+        try {
+            FileInputStream fis = context.openFileInput(FILENAME);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            itemsList = (ArrayList<String>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            itemsList = new ArrayList<>();
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return itemsList;
+    }
+}
